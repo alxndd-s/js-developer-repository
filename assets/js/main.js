1ec38bca7e239e0c353a143f2d41f6b9b0a89a3f
Alexandre Rodrigues <alexandrerodrigues31@live.com>
2023-12-10 07:46:40 -0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,38 @@
+
+function updateProfileInfo(profileData){
+
+    const profile = profileData
+    setInterval(() => {
+        const photo = document.getElementById('profile.photo')
+        photo.src = profile.photo
+        photo.alt = "Foto de " + profile.name
+    
+        const name = document.getElementById('profile.name')
+        name.textContent = profile.name
+    
+        const job = document.getElementById('profile.job')
+        job.textContent = profile.job
+    
+        
+        const location = document.getElementById('profile.location')
+        location.textContent = profile.location
+    
+        const phone = document.getElementById('profile.phone')
+        phone.textContent = profile.phone
+    
+        const email = document.getElementById('profile.email')
+        email.textContent = profile.email
+    },300)
+
+
+}
+
+
+
+
+
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileInfo(profileData)
 })()
